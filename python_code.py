16d4e0142cc73b3bc64c20b7b3d4d13cf3ab6865
PengjunGuo813 <59590524+PengjunGuo813@users.noreply.github.com>
2020-06-25 22:48:05 -0400

    adds a python script

---
+++ python_code.py
@@ -0,0 +1,6 @@
+## This notebook This is used to train how to push a notebook to a Github repository.
+## Let's import some libraries.
+import pandas as pd
+import numpy as np
+## Try to print a statement.
+print("Hello Capstone Project Course!")
